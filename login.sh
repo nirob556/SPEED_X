7bbb8add7020db0ddeaed3bd71013482a117ca11
SPEED_X <nirobhhosan@gmail.com>
2025-06-02 15:38:42 +0600

    login.sh

--- login.sh
+++ login.sh
@@ -57,7 +57,7 @@ echo -e "\033[1m\033[33m
 ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝                                                                    
 
 "
-echo -e  "     \e[1m\e[32m▂▃▄▅▆▇▓▒░ \033[1mCoded By \e[33mCyberAK \e[1m\e[32m░▒▓▇▆▅▄▃▂"
+echo -e  "     \e[1m\e[32m▂▃▄▅▆▇▓▒░ \033[1mCoded By \e[33mGHOST X MODS @JOBAYAR_AHMED @SPEED_X_OWNER 🧑‍💻☠️ @ \e[1m\e[32m░▒▓▇▆▅▄▃▂"
 cd $HOME
 echo -e "   \033[1m\033[33m]\033[31m──────────────────────────────────────\033[33m["
 echo 
